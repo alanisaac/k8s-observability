ba8254535a7f817e0afcb1833efc3194f6ddf79b
Alan Pinkert <alan.pinkert@gmail.com>
2021-03-01 09:54:41 -0500

    fixed jaeger configuration

--- service/src/ServiceCollectionExtensions.cs
+++ service/src/ServiceCollectionExtensions.cs
@@ -1,30 +1,24 @@
-using System;
 using Jaeger;
-using Jaeger.Samplers;
 using Jaeger.Senders;
 using Jaeger.Senders.Thrift;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using OpenTracing;
 
 namespace MyApp
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddJaeger(this IServiceCollection serviceCollection, string serviceName)
+        public static void AddJaeger(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddSingleton<ITracer>(x => 
+            serviceCollection.AddSingleton(x => 
             {
                 var loggerFactory = x.GetRequiredService<ILoggerFactory>();
 
                 Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
 	                .RegisterSenderFactory<ThriftSenderFactory>();
 
-                var tracer = new Tracer.Builder(serviceName)
-                    .WithLoggerFactory(loggerFactory)
-                    .WithSampler(new ConstSampler(true))
-                    .Build();
-                    
+                var config = Configuration.FromEnv(loggerFactory);
+                var tracer = config.GetTracer();
                 return tracer;
             });
         }
--- service/src/Startup.cs
+++ service/src/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using OpenTracing;
 
 namespace MyApp
 {
@@ -29,7 +21,7 @@ namespace MyApp
             services.AddControllers();
             services.AddHealthChecks();
             services.AddOpenTracing();
-            services.AddJaeger("my-app");
+            services.AddJaeger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
